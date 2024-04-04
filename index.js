7f839b31440c84ecbf1430fc814e1e819a2452ae
DosDima <dosdima89@gmail.com>
2024-04-04 13:51:57 +0300

    work

--- index.js
+++ index.js
@@ -1,34 +1,86 @@
-import puppeteer from 'puppeteer';
+import puppeteer from "puppeteer";
+import fs from "node:fs";
+
+const createMachinery = async (id, type, brand, year, model, productIds) => {
+  fs.appendFileSync(
+    `./files/moto.json`,
+    `${id} | ${type} | ${brand} | ${year} | ${model} | ${productIds}\n`
+  ),
+    (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.error("file written successfully");
+      }
+    };
+};
 
 (async () => {
-    // Launch the browser and open a new blank page
+  try {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
+    await page.setViewport({ width: 1080, height: 3000 });
 
-    // Navigate the page to a URL
-    await page.goto('https://developer.chrome.com/');
+    for (let i = 44600; i < 100000; i++) {
+      console.log(`${i}`);
 
-    // Set screen size
-    await page.setViewport({ width: 1080, height: 1024 });
+      await page.goto(`https://general-moto.ru/models/?model_id=${i}`);
 
-    await page.screenshot({ path: './screen/example.png'})
+      const factories = await page.$$eval(
+        "#gm-factories > select > option",
+        (option) => {
+          return option.map((anchor) => anchor.textContent);
+        }
+      );
 
-    // // Type into search box
-    // await page.type('.devsite-search-field', 'automate beyond recorder');
+      if (factories[0] === "- Производитель -" || !factories[0]) continue;
 
-    // // Wait and click on first result
-    // const searchResultSelector = '.devsite-result-item-link';
-    // await page.waitForSelector(searchResultSelector);
-    // await page.click(searchResultSelector);
+      const year = await page.$$eval("#gm-years > select", (option) => {
+        return option.map((anchor) => {
+          return option.map((anchor) => anchor.textContent);
+        });
+      });
 
-    // // Locate the full title with a unique string
-    // const textSelector = await page.waitForSelector(
-    //     'text/Customize and automate'
-    // );
-    // const fullTitle = await textSelector?.evaluate(el => el.textContent);
+      const model = await page.$$eval(
+        "#gm-models > select > option",
+        (option) => {
+          return option.map((anchor) => anchor.textContent);
+        }
+      );
 
-    // // Print the full title
-    // console.log('The title of this blog post is "%s".', fullTitle);
+      const links = await page.evaluate(() => {
+        const selector = `.offers > form > input[name=product_id]`;
+        return Array.from(document.querySelectorAll(selector), (e) => e.value);
+      });
+
+      const typeDOM = await page.waitForSelector("#page-content > h1");
+      const value = await typeDOM.evaluate((el) => el.textContent);
+
+      let type = "мотоцикл";
+      
+      if (value.includes("мотоцикла")) {
+        type = "мотоцикл";
+      }
+
+      if (value.includes("квадроцикла")) {
+        type = "квадроцикл";
+      }
+
+      if (value.includes("скутера")) {
+        type = "скутер";
+      }
+      
+      if (value.includes("снегохода")) {
+        type = "снегоход";
+      }
+      
+      createMachinery(i, type, factories[0], year[0], model[0], links);
+
+      console.log(`${factories[0]} | ${year[0]} | ${model[0]}`);
+    }
 
     await browser.close();
+  } catch (err) {
+    console.log(err);
+  }
 })();
